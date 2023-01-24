4d9940137aaf3b554790bf6093abe7ca13922b7e
m100bit <37666243+m100bit@users.noreply.github.com>
2023-01-24 14:37:05 +0300

    some kind of important changes

--- demozoo-parsing.sh
+++ demozoo-parsing.sh
@@ -12,10 +12,16 @@ fi
 
 echo -e "\033[0;33mInstalling Python3:\033[0m"
 if ! [ -x "$(command -v python3)" ]; then
-  sudo $PACKAGE_MANAGER python3 pip3 > /dev/null
+  sudo $PACKAGE_MANAGER python3 > /dev/null
   echo -e "\033[0;32m Python3 has been installed!\033[0m"
 else
   echo -e "\033[0;32m Python3 is already installed.\033[0m"
+fi
+if ! [ -x "$(command -v pip3)" ]; then
+  sudo $PACKAGE_MANAGER python3-pip > /dev/null
+  echo -e "\033[0;32m Pip3 has been installed!\033[0m"
+else
+  echo -e "\033[0;32m Pip3 is already installed.\033[0m"
 fi
 
 echo -e "\033[0;33mInstalling 'pandas' with pip:\033[0m"
@@ -66,28 +72,28 @@ if [[ ! -e ~/demozoo ]]; then
 else
   echo -e "\033[0;32m Folder 'demozoo' already exists in your home directory. This is the main folder for this project.\033[0m"
 fi
-cd ~/demozoo
+cd /tmp
 echo -e "\033[0;33m Downloading latest version of the demozoo-export.sql.gz... it might take a while.\033[0m"
 wget -q https://data.demozoo.org/demozoo-export.sql.gz
 echo -e "\033[0;33m Downloading latest version of the Demozoo Python parsing script... should be very quick.\033[0m"
 wget -q -O csv.py https://raw.githubusercontent.com/m100bit/demozoo-parsing-experiments/main/scripts/csv.py
+cd ~/demozoo
 wget -q -O parsing.py https://raw.githubusercontent.com/m100bit/demozoo-parsing-experiments/main/scripts/parsing.py
 echo -e "\033[0;32m  Finished downloading!\033[0m"
 
 echo -e "\033[0;33mUnpacking the SQL file. It also might take a while...\033[0m"
-gunzip demozoo-export.sql.gz
+cd /tmp
+yes | gunzip demozoo-export.sql.gz
 echo -e "\033[0;32m File has been unpacked!"
-ls demozoo-export.sql
-echo -e "\033[0m"
 
 echo -e "\033[0;33mScript that will copy needed table to the .csv file will be executed.\033[0m"
 echo -e "\033[0;31mPLEASE MAKE SURE YOU DON'T HAVE SOMETHING IMPORTANT IN THE DATABASE CALLED \033[0mdemozoo_test \033[0;31mIN POSTGRESQL!\033[0m"
 read -p "Press Enter to continue..."
 echo -e "\033[0;33mExecuting script... This might take a few minutes. Open Demozoo and watch your favourite demos while waiting.\033[0m"
-sudo su -c "python3 csv.py" postgres > /dev/null
+sudo su postgres -c "python3 csv.py" > /dev/null
 echo -e "\033[0;32m .csv file was generated somewhere in /tmp.\033[0m"
 echo -e "\033[0;33m Moving it...\033[0m"
-sudo mv /tmp/prods.csv ~/demozoo
+sudo mv prods.csv ~/demozoo
 echo -e "\033[0;33m ...removing .sql file...\033[0m"
 rm demozoo-export.sql
 echo -e "\033[0;33m ...and .csv script (it may cause problems)...\033[0m"
@@ -95,6 +101,7 @@ rm csv.py
 echo -e "\033[0;32m  Done!\033[0m"
 echo ""
 
+cd ~/demozoo
 echo -e "\033[0;33mScript is ready to be executed. Just type\033[0m"
 echo "cd ~/demozoo"
 echo "python3 parsing.py"
