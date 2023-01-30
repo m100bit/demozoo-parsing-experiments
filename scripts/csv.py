ab346a67d80ec7bf1be6b0e474316b85558bd347
m100bit <37666243+m100bit@users.noreply.github.com>
2023-01-30 21:25:08 +0300

    "lost" prods are gone

--- scripts/csv.py
+++ scripts/csv.py
@@ -2,18 +2,21 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import psycopg2
 import os
 
-con = psycopg2.connect("user=postgres password=postgres") #initial connect to postgresql
+con = psycopg2.connect("user=postgres password=postgres") # initial connect to postgresql
 con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = con.cursor()
 
 cursor.execute("DROP DATABASE IF EXISTS demozoo_test;")
-cursor.execute("CREATE DATABASE demozoo_test;") #recreating database to load an export
+cursor.execute("CREATE DATABASE demozoo_test;") # recreating database to load an export
 
-os.system('psql -U postgres demozoo_test < demozoo-export.sql') #importing sql export to the new database
+os.system('psql -U postgres demozoo_test < demozoo-export.sql') # importing sql export to the new database
 
 con = psycopg2.connect("user=postgres password=postgres dbname=demozoo_test")
 con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cursor = con.cursor() #reconnecting to the new database
+cursor = con.cursor() # reconnecting to the new database
 
 os.system('touch /tmp/prods.csv')
-cursor.execute("COPY productions_productionlink TO '/tmp/prods.csv' WITH CSV HEADER;") #copying table to the file
+cursor.execute("DROP TABLE IF EXISTS temp;")
+cursor.execute("CREATE TABLE temp AS SELECT * FROM productions_productionlink INNER JOIN (SELECT tag_id, object_id, content_type_id FROM taggit_taggeditem) taggit ON productions_productionlink.production_id = taggit.object_id;") # joining prods and tags
+cursor.execute("DELETE FROM temp WHERE production_id IN (SELECT production_id FROM temp WHERE tag_id = 500);") # removing all "lost" prods
+cursor.execute("COPY temp TO '/tmp/prods.csv' WITH CSV HEADER;") # copying table to the file
