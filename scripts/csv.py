b1cb6eb6194555ed5802c81901d0b4c25ba063c0
m100bit <37666243+m100bit@users.noreply.github.com>
2023-02-17 22:22:39 +0300

    reverting back

--- scripts/csv.py
+++ scripts/csv.py
@@ -13,10 +13,13 @@ os.system('psql -U postgres demozoo_test < demozoo-export.sql') # importing sql 
 
 con = psycopg2.connect("user=postgres password=postgres dbname=demozoo_test")
 con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cursor = con.cursor() # reconnecting to the new database
+cursor = con.cursor() #reconnecting to the new database
 
 os.system('touch /tmp/prods.csv')
-cursor.execute("DROP TABLE IF EXISTS temp;")
-cursor.execute("CREATE TABLE temp AS SELECT * FROM productions_productionlink INNER JOIN (SELECT tag_id, object_id, content_type_id FROM taggit_taggeditem) taggit ON productions_productionlink.production_id = taggit.object_id;") # joining prods and tags
-cursor.execute("DELETE FROM temp WHERE production_id IN (SELECT production_id FROM temp WHERE tag_id = 500);") # removing all "lost" prods
-cursor.execute("COPY temp TO '/tmp/prods.csv' WITH CSV HEADER;") # copying table to the file
+cursor.execute("COPY productions_productionlink TO '/tmp/prods.csv' WITH CSV HEADER;") #copying table to the file
+
+#DOESN'T WORK FOR NOW
+#cursor.execute("DROP TABLE IF EXISTS temp;")
+#cursor.execute("CREATE TABLE temp AS SELECT * FROM productions_productionlink INNER JOIN (SELECT tag_id, object_id, content_type_id FROM taggit_taggeditem) taggit ON productions_productionlink.production_id = taggit.object_id;") # joining prods and tags
+#cursor.execute("DELETE FROM temp WHERE production_id IN (SELECT production_id FROM temp WHERE tag_id = 500);") # removing all "lost" prods
+#cursor.execute("COPY temp TO '/tmp/prods.csv' WITH CSV HEADER;") # copying table to the file
